9b5741bc7c8a218adfc749505ad236bfcfb2827c
Bibek1008 <bibekbhandari9848@gmail.com>
2026-05-05 21:17:01 +0530

    production failed resolved

--- backend/index.js
+++ backend/index.js
@@ -66,9 +66,9 @@ app.use("/api/messages", messageRouter);
 const PORT = process.env.PORT;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
   });
 }
 
